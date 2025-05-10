cf014bdf6d0efb336da527f701ea1203e002f75f
bixiphel <allen131@rangers.uwp.edu>
2025-05-09 21:13:20 -0500

    Addded underlying data structure as well as Edge and Node inner classes, which will be later used to implement LocationGraph methods.

--- src/main/java/edu/uwp/cs/csci340/assignments/gtfh/LocationGraph.java
+++ src/main/java/edu/uwp/cs/csci340/assignments/gtfh/LocationGraph.java
@@ -1,4 +1,37 @@
 package edu.uwp.cs.csci340.assignments.gtfh;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class LocationGraph {
+
+    // I chose to use a Map as the underlying data structure for my adjacency list
+    // Probably should use generics here, but I didn't want to include extra handling since it's not in the scope of the assignment.
+    private final Map<String, List<Edge>> adjacencyList = new HashMap<>();
+
+    /**
+     * Inner class for an Edge object. It contains a destination and the length to it
+     */
+    private static class Edge {
+        private String to;
+        private double length;
+
+        // Constructor for an Edge object
+        Edge(String from, String to, double length) {
+            this.to = to;
+            this.length = length;
+        }
+    }
+
+    /**
+     * Private Inner class for a Node object, which contains a name label
+     */
+    private static class Node {
+        private String name;
+
+        Node(String name, double distance) {
+            this.name = name;
+        }
+    }
 }
